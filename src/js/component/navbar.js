5834810490df736e3a1bc09c8f58dded9f1d5f3d
Navarroseb <sebastnavarro@gmail.com>
2022-02-15 12:53:14 +0000

    navbar avanzado

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-/* import images from "../../img"; */ 
+import starwarslogo from "../../img/Starwars_logo.png";
+import darthvader from "../../img/Darth_Vader.png";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="/workspace/react-hello-webapp/src/img/Starwars_logo.png" alt="" width="30" height="24"/>
-    </a>
-  </div>
-  <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle bg-primary text-ligth" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Favorites
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-</nav>
+		<nav class="navbar navbar-light bg-light justify-content-end">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src={starwarslogo} alt="" width="60" height="60" />
+					<img src={darthvader} alt="" width="60" height="60" />
+				</a>
+			</div>
+			<div>
+				<a class="nav-link dropdown-toggle bg-primary text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites
+				</a>
+				<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<li><a class="dropdown-item" href="#">Favorites</a></li>
+				</ul>
+			</div >
+		</nav>
 	);
 };
