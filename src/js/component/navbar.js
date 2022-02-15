2338300f332b278efba473da43163af9638b930e
Navarroseb <sebastnavarro@gmail.com>
2022-02-15 11:57:06 +0000

    Iniciando proyecto

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+/* import images from "../../img"; */
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="" alt="" width="30" height="24"/>
+    </a>
+  </div>
+  <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle bg-primary text-ligth" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Favorites
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+</nav>
 	);
 };
